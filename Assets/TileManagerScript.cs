6c61d3bfa3efe18d25c087210a2154008c79b245
CLASSICALJOKE\classicaljoke <naimiskandar22@gmail.com>
2018-01-27 03:56:17 +0800

    Create Tiles

---
+++ Assets/TileManagerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManagerScript : MonoBehaviour {
+
+	public static TileManagerScript instance;
+
+	//Temp mat boost variable for player
+	public float matChangeSpeed = 5f;
+	public float matUpdateValue = 50f;
+
+	//Temp variable
+	//public bool curGunMode = false;
+
+	//Temporary tile array. Will be replaced by array of sprites
+	public Sprite[] tileTypes;
+	public GameObject tilePrefab;
+
+	public List<TileScript> tileList = new List<TileScript>();
+
+	void Awake()
+	{
+		if(instance == null) instance = this;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+
+		for(int i = 0; i < tiles.Length; i++)
+		{
+			tileList.Add(tiles[i].GetComponent<TileScript>());
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
---
+++ Assets/TileScript.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public enum TileType
+{
+	GLASS = 0,
+	WOOD,
+	BRICK,
+	CONCRETE,
+	METAL,
+	TOTAL,
+};
+
+public class TileScript : MonoBehaviour {
+
+	public bool isAlive;
+	public bool itemSpawned;
+
+	//Discuss with Eugene
+	public bool matCanChange = true;
+
+	SpriteRenderer rend;
+	BoxCollider2D myColl;
+
+	public TileType type;
+	public int fragValue;
+
+	public float curMatValue;
+	float nextMatValue;
+	float prevMatValue;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<SpriteRenderer>();
+		myColl = GetComponent<BoxCollider2D>();
+
+		UpdateMat();
+
+		curMatValue = (int)type * TileManagerScript.instance.matUpdateValue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Physics2D.OverlapBoxAll(transform.position, 1f, 0f);
+
+	}
+
+	public void UpdateMat()
+	{
+		int type_int = (int)type;
+
+		rend.sprite = TileManagerScript.instance.tileTypes[type_int];
+
+
+		if(type_int >= (int)TileType.METAL)
+		{
+			matCanChange = false;
+		}
+		else if(type_int == 0)
+		{
+			matCanChange = false;
+		}
+
+		nextMatValue = (type_int + 1) * TileManagerScript.instance.matUpdateValue;
+		prevMatValue = (type_int) * TileManagerScript.instance.matUpdateValue;
+
+	}
+
+	void OnMouseOver()
+	{
+		if(Input.GetButton("Fire1"))
+		{
+			if(matCanChange)
+			{
+				curMatValue += Time.deltaTime * TileManagerScript.instance.matChangeSpeed;
+
+				if(curMatValue >= nextMatValue)
+				{
+					int tempType = (int)type + 1;
+					type = (TileType)tempType;
+					UpdateMat();
+				}
+			}
+
+		}
+		else if(Input.GetButton("Fire2"))
+		{
+			if(matCanChange)
+			{
+				curMatValue -= Time.deltaTime * TileManagerScript.instance.matChangeSpeed;
+
+				if(curMatValue <= prevMatValue)
+				{
+					int tempType = (int)type - 1;
+					type = (TileType)tempType;
+					UpdateMat();
+				}
+			}
+		}
+	}
+
+
+	public void SetAlive(bool alive)
+	{
+		isAlive = alive;
+		rend.enabled = alive;
+		myColl.isTrigger = alive;
+
+	}
+}
