7220819dff090e4ed62ebb1824b602693861a772
FattyMieo <fong2009@live.com.my>
2018-01-28 16:53:18 +0800

    reworking

--- Assets/Scripts/Managers/ItemSpawnManagerScript.cs
+++ Assets/Scripts/Managers/ItemSpawnManagerScript.cs
@@ -21,21 +21,27 @@ public class ItemSpawnManagerScript : MonoBehaviour
 	public GameObject itemPrefab;
 	public PlayerScript player;
 
+	float timer;
+	float duration = 10f;
+
 	void Awake()
 	{
 		if(instance == null) instance = this;
 	}
 
+	void Start()
+	{
+		timer = duration;
+	}
+
 	void Update()
 	{
-		if(Input.GetKeyDown(KeyCode.Space))
+		timer -= Time.deltaTime;
+
+		if(timer <= 0f)
 		{
 			SpawnItem();
-		}
-
-		if(Input.GetKeyDown(KeyCode.Return))
-		{
-			RegenNeighTile();
+			timer = duration;
 		}
 	}
 
