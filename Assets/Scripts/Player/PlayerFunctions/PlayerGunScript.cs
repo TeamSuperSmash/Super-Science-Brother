e18062916bce3923e06bb6421d2a781fc6f8f0ef
TerrenceChan <roxes2112@yahoo.com>
2018-02-19 18:02:15 +0800

    *Added line renderer to show laser attack feedback. -[BUG] Line renderer will shoot backwards when mouse shoots near the center of the player.
    *Fixed gun aiming rotation.

---
+++ Assets/Scripts/Player/PlayerFunctions/PlayerGunScript.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGunScript : MonoBehaviour
+{
+    [Header("Settings")]
+    public LayerMask layerToHit;
+    PlayerScript player;
+
+    [Header("Stats")]
+    public bool usingMassGun;
+    public bool isDraining;
+    [TooltipAttribute("Mass Change Per Hit")]
+    public float massTransferRate = 10.0f;
+
+    [Header("Force Gun Stats")]
+    public bool isCooldownForceGun = false;
+    public float forceGunFactor = 0.0f;
+    public float forceGunCooldown = 2.0f;
+    public float forceGunTimer = 0.0f;
+
+    [Header("Prefabs")]
+    public GameObject forceBallPrefab;
+
+    [Header("Counter For Sounds")]
+    float sfxTimer0 = 1.0f;
+    float sfxTimer1 = 1.0f;
+
+    [Header("Fire Position")]
+    public Transform laserFireOutput;
+    public Transform forceFireOutput;
+
+    [Header("Laser Graphic")]
+    public LineRenderer laserRender;
+
+    private void Start()
+    {
+        player = GetComponentInParent<PlayerScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckMassGun();
+        CheckForceGun();
+    }
+
+    void CheckMassGun()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            usingMassGun = true;
+            isDraining = false;
+
+            sfxTimer1 += Time.deltaTime;
+
+            if (sfxTimer1 > sfxTimer0)
+            {
+                sfxTimer1 = 0.0f;
+                SoundManagerScript.Instance.PlaySFX(AudioClipID.SFX_MASSGUN1);
+            }
+
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            usingMassGun = true;
+            isDraining = true;
+
+            sfxTimer1 += Time.deltaTime;
+
+            if (sfxTimer1 > sfxTimer0)
+            {
+                sfxTimer1 = 0.0f;
+                SoundManagerScript.Instance.PlaySFX(AudioClipID.SFX_MASSGUN2);
+            }
+        }
+        else
+        {
+            usingMassGun = false;
+        }
+
+        if (usingMassGun)
+        {
+            laserRender.enabled = true;
+            MassShoot();
+        }
+        else
+        {
+            laserRender.enabled = false;
+            return;
+        }
+    }
+
+    void CheckForceGun()
+    {
+        if (!isCooldownForceGun)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                forceGunTimer = forceGunCooldown;
+                isCooldownForceGun = true;
+
+                ForceShoot();
+
+                SoundManagerScript.Instance.PlaySFX(AudioClipID.SFX_FORCEPUSH);
+            }
+        }
+        else
+        {
+            forceGunTimer -= Time.deltaTime;
+            if (forceGunTimer <= 0.0f)
+            {
+                forceGunTimer = forceGunCooldown;
+                isCooldownForceGun = false;
+            }
+        }
+    }
+
+    void MassShoot()
+    {
+        Vector2 firePointPosition = new Vector2(laserFireOutput.transform.position.x, laserFireOutput.transform.position.y);
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, transform.right, 100.0f, layerToHit);
+
+        laserRender.startColor = isDraining ? Color.blue : Color.red;
+        laserRender.endColor = isDraining ? Color.cyan : Color.yellow;
+
+        laserRender.SetPosition(0, laserFireOutput.position);
+
+        // Need to solve the bug where the laser would shoot backwards!!!
+        laserRender.SetPosition(1, mousePosition);
+
+        //Debug.DrawLine (firePointPosition, transform.right, (isDraining ? Color.green : Color.yellow));
+        //Debug.Log ("Player shoot the lazer!");
+
+        if (hit)
+        {
+            laserRender.SetPosition(0, laserFireOutput.position);
+            laserRender.SetPosition(1, hit.point);
+
+            //Debug.DrawLine (firePointPosition, hit.point, Color.red);
+
+            if (hit.collider.CompareTag("Player"))
+            {
+                PlayerScript targetPlayer = hit.collider.gameObject.GetComponentInParent<PlayerScript>();
+
+                if (isDraining)
+                {
+                    if (player.mass <= PlayerSettings.maxMass && targetPlayer.mass >= PlayerSettings.minMass)
+                    {
+                        targetPlayer.mass -= massTransferRate * Time.deltaTime;
+                        player.mass += massTransferRate * Time.deltaTime;
+                    }
+                }
+                else
+                {
+                    if (player.mass >= PlayerSettings.minMass && targetPlayer.mass <= PlayerSettings.maxMass)
+                    {
+                        targetPlayer.mass += massTransferRate * Time.deltaTime;
+                        player.mass -= massTransferRate * Time.deltaTime;
+                    }
+                }
+            }
+            else if (hit.collider.CompareTag("Tile"))
+            {
+                TileScript targetTile = hit.collider.gameObject.GetComponent<TileScript>();
+
+                if (isDraining)
+                {
+                    if (player.mass <= PlayerSettings.maxMass && targetTile.mass >= PlayerSettings.minMass)
+                    {
+                        targetTile.mass -= massTransferRate * Time.deltaTime;
+                        player.mass += massTransferRate * Time.deltaTime;
+                    }
+                }
+                else
+                {
+                    if (player.mass >= PlayerSettings.minMass && targetTile.mass <= PlayerSettings.maxMass)
+                    {
+                        targetTile.mass += massTransferRate * Time.deltaTime;
+                        player.mass -= massTransferRate * Time.deltaTime;
+                    }
+                }
+            }
+        }
+    }
+
+    void ForceShoot()
+    {
+        Instantiate(forceBallPrefab, forceFireOutput.transform.position, Quaternion.identity);
+    }
+}
--- Assets/Scripts/Projectiles/ForceGunProjectile.cs
+++ Assets/Scripts/Projectiles/ForceGunProjectile.cs
@@ -4,73 +4,77 @@ using UnityEngine;
 
 public class ForceGunProjectile : MonoBehaviour
 {
-	public float forceAmplify = 10.0f;
-	public float radius = 100.0f;
+    public float forceAmplify = 10.0f;
+    public float radius = 100.0f;
 
-	public float lifeTime = 0.1f;
-	public float lifeTimeCounter = 0.0f;
+    public float lifeTime = 0.1f;
+    public float lifeTimeCounter = 0.0f;
 
-    void Update ()
-	{
-		CheckLifeTime ();
-	}
+    void Update()
+    {
+        CheckLifeTime();
+    }
 
-	void AddExplosionForce2D (Rigidbody2D body, float explodeForce, Vector3 explodePos, float explodeRadius)
-	{
-		Vector3 dir = (body.transform.position - explodePos);
-		float calculate = 1 - (dir.magnitude / explodeRadius);
+    void AddExplosionForce2D(Rigidbody2D body, float explodeForce, Vector3 explodePos, float explodeRadius)
+    {
+        Vector3 dir = (body.transform.position - explodePos);
+        float calculate = 1 - (dir.magnitude / explodeRadius);
 
-		if (calculate < 0) {
-			calculate = 0;
-		}
+        if (calculate < 0)
+        {
+            calculate = 0;
+        }
 
-		body.AddForce (dir.normalized * explodeForce * calculate);
-	}
+        body.AddForce(dir.normalized * explodeForce * calculate);
+    }
 
-	void OnTriggerEnter2D (Collider2D other)
-	{
-		Rigidbody2D tempRb2d = other.GetComponent<Rigidbody2D> ();
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Rigidbody2D tempRb2d = other.GetComponent<Rigidbody2D>();
 
-		if (tempRb2d) {
-			float power = 0.0f;
+        if (tempRb2d)
+        {
+            float power = 0.0f;
 
-			if (other.gameObject.CompareTag ("Player"))
+            if (other.gameObject.CompareTag("Player"))
             {
-				Debug.Log ("Player toucha the exploda!");
+                Debug.Log("Player toucha the exploda!");
                 SoundManagerScript.Instance.PlaySFX(AudioClipID.SFX_GETTINGPUSH);
 
-                PlayerScript tempPlayer = other.gameObject.GetComponent<PlayerScript> ();
+                PlayerScript tempPlayer = other.gameObject.GetComponent<PlayerScript>();
                 PlayerGunScript tempGun = other.gameObject.GetComponentInChildren<PlayerGunScript>();
 
                 // Power is inverse to the player's mass.
                 power = ((-tempPlayer.mass + 250.0f) * forceAmplify) + tempGun.forceGunFactor;
-			}
+            }
             else
             {
-				power = 250.0f * forceAmplify;
-			}
+                power = 250.0f * forceAmplify;
+            }
 
-			if (power < 0.0f) {
-				power = 0.0f;
-			}
+            if (power < 0.0f)
+            {
+                power = 0.0f;
+            }
 
             // Reset velocity to improve consistency.
-			Vector2 v = tempRb2d.velocity;
-			v.y = 0.0f;
-			tempRb2d.velocity = v;
+            Vector2 v = tempRb2d.velocity;
+            v.y = 0.0f;
+            tempRb2d.velocity = v;
 
-			AddExplosionForce2D (tempRb2d, power, transform.position, radius);
-		}
-	}
+            AddExplosionForce2D(tempRb2d, power, transform.position, radius);
+        }
+    }
 
-	void CheckLifeTime ()
-	{
-		lifeTimeCounter += Time.deltaTime;
+    void CheckLifeTime()
+    {
+        lifeTimeCounter += Time.deltaTime;
 
-		if (lifeTimeCounter > lifeTime) {
-			lifeTimeCounter = 0.0f;
+        if (lifeTimeCounter > lifeTime)
+        {
+            lifeTimeCounter = 0.0f;
 
-			Destroy (this.gameObject);
-		}
-	}
+            Destroy(this.gameObject);
+        }
+    }
 }
